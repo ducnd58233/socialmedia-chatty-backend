ef233691c45a1f3c6764e106a5afc60feeb75a2c
ducnd58233 <only4wns@gmail.com>
2022-11-15 01:02:03 +0700

    feature: remove reaction from database

--- src/shared/services/db/reactions.service.ts
+++ src/shared/services/db/reactions.service.ts
@@ -1,4 +1,4 @@
-import { IPostDocument } from '@post/interfaces/post.interface';
+import { IPostDocument } from '@post/interfaces/post.interface'
 import { PostModel } from '@post/models/post.schema'
 import { IReactionDocument, IReactionJob } from '@reaction/interfaces/reactions.interface'
 import { ReactionModel } from '@reaction/models/reactions.schema'
@@ -10,9 +10,9 @@ const userCace: UserCache = new UserCache()
 class ReactionService {
   public async addReactionDataToDB(reactionData: IReactionJob): Promise<void> {
     const { postId, userTo, userFrom, username, type, previousReaction, reactionObject } = reactionData
-    const updatedReaction: [IUserDocument, IReactionDocument, IPostDocument] = await Promise.all([
+    const updatedReaction: [IUserDocument, IReactionDocument, IPostDocument] = (await Promise.all([
       userCace.getUserFromCache(`${userTo}`),
-      ReactionModel.replaceOne({ postId, type: previousReaction, username: username }, reactionObject, { upsert: true }),
+      ReactionModel.replaceOne({ postId, type: previousReaction, username }, reactionObject, { upsert: true }),
       PostModel.findOneAndUpdate(
         { _id: postId },
         {
@@ -23,9 +23,25 @@ class ReactionService {
         },
         { new: true }
       )
-    ]) as unknown as [IUserDocument, IReactionDocument, IPostDocument]
+    ])) as unknown as [IUserDocument, IReactionDocument, IPostDocument]
 
     // send reactions notification
+  }
+
+  public async removeReactionDataFromDB(reactionData: IReactionJob): Promise<void> {
+    const { postId, previousReaction, username } = reactionData
+    await Promise.all([
+      ReactionModel.deleteOne({ postId, type: previousReaction, username }),
+      PostModel.updateOne(
+        { _id: postId },
+        {
+          $inc: {
+            [`reactions.${previousReaction}`]: -1
+          }
+        },
+        { new: true }
+      )
+    ])
   }
 }
 
