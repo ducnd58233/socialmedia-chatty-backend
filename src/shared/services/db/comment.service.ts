8e2157e4421e66d5ad8a597ef875c86637a779cd
ducnd58233 <only4wns@gmail.com>
2022-12-01 11:08:41 +0700

    feature: comment redis cache methods

--- src/shared/services/db/comment.service.ts
+++ src/shared/services/db/comment.service.ts
@@ -1,7 +1,48 @@
-import { ICommentJob } from '@comment/interfaces/comment.interface'
+import { Query } from 'mongoose'
+import { IQueryComment, ICommentNameList, ICommentDocument, ICommentJob } from '@comment/interfaces/comment.interface'
+import { CommentsModel } from '@comment/models/comment.schema'
+import { PostModel } from '@post/models/post.schema'
+import { IPostDocument } from '@post/interfaces/post.interface'
+import { UserCache } from '@service/redis/user.cache'
+import { IUserDocument } from '@user/interfaces/user.interface'
+
+const userCache: UserCache = new UserCache()
 
 class CommentService {
-  public async addCommentToDB(commentData: ICommentJob)
+  public async addCommentToDB(commentData: ICommentJob): Promise<void> {
+    const { postId, userTo, userFrom, comment, username } = commentData
+    const comments: Promise<ICommentDocument> = CommentsModel.create(comment)
+    const post: Query<IPostDocument, IPostDocument> = PostModel.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { commentsCount: 1 } },
+      { new: true }
+    ) as Query<IPostDocument, IPostDocument>
+
+    const user: Promise<IUserDocument> = userCache.getUserFromCache(userTo) as Promise<IUserDocument>
+    const response: [ICommentDocument, IPostDocument, IUserDocument] = await Promise.all([comments, post, user])
+
+    // send comments notification
+  }
+
+  public async getPostCommentsFromDB(query: IQueryComment, sort: Record<string, 1 | -1>): Promise<ICommentDocument[]> {
+    const comments: ICommentDocument[] = await CommentsModel.aggregate([
+      { $match: query },
+      { $sort: sort }
+    ])
+
+    return comments
+  }
+
+  public async getPostCommentsNamesFromDB(query: IQueryComment, sort: Record<string, 1 | -1>): Promise<ICommentNameList[]> {
+    const commentsNamesList: ICommentNameList[] = await CommentsModel.aggregate([
+      { $match: query },
+      { $sort: sort },
+      { $group: { _id: null, names: { $addToSet: '$username' }, count: { $sum: 1 } } }, // seperate data into group keys
+      { $project: { _id: 0 } } // exclude the id from final result
+    ])
+
+    return commentsNamesList
+  }
 }
 
 export const commentService: CommentService = new CommentService()
