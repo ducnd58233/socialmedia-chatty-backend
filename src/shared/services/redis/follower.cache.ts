cb344296aaf05f33261d5c364cd89750414acb2d
ducnd58233 <only4wns@gmail.com>
2022-12-05 09:27:30 +0700

    feature: follower following redis methods

---
+++ src/shared/services/redis/follower.cache.ts
@@ -0,0 +1,45 @@
+import { ServerError } from '@global/helpers/error-handlers'
+import { BaseCache } from '@service/redis/base.cache'
+import { config } from '@root/config'
+import Logger from 'bunyan'
+
+const log: Logger = config.createLogger('followersCache')
+
+export class FollowerCache extends BaseCache {
+  constructor() {
+    super('followersCache')
+  }
+
+  public async saveFollowerToCache(key: string, value: string): Promise<void> {
+    try {
+      if (!this.client.isOpen) await this.client.connect()
+
+      await this.client.LPUSH(key, value)
+    } catch (error) {
+      log.error(error)
+      throw new ServerError('Server error. Try again.')
+    }
+  }
+
+  public async removeFollowerFromCache(key: string, value: string): Promise<void> {
+    try {
+      if (!this.client.isOpen) await this.client.connect()
+
+      await this.client.LREM(key, 1, value)
+    } catch (error) {
+      log.error(error)
+      throw new ServerError('Server error. Try again.')
+    }
+  }
+
+  public async updateFollowersCountInCache(userId: string, prop: string, value: number): Promise<void> {
+    try {
+      if (!this.client.isOpen) await this.client.connect()
+
+      await this.client.HINCRBY(`users:${userId}`, prop, value)
+    } catch (error) {
+      log.error(error)
+      throw new ServerError('Server error. Try again.')
+    }
+  }
+}
